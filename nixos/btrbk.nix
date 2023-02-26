897707d0cb41a22feea192476b662f790bcf5969
rkotkiewicz <you@example.com>
2023-02-26 18:45:18 +0100

    disable btrbk /etc/nixos snapshots

--- nixos/btrbk.nix
+++ nixos/btrbk.nix
@@ -1,5 +1,10 @@
 { config, pkgs, ... }: {
 
+# to reduce the size of btrbk snapshots, create btrfs subvolumes, e.g.
+# @home/<user>/.local/share/Steam
+# @home/<user>/.cache
+# @home/<user>/.local/share/baloo
+
   services.btrbk = {
     instances."btrbk" = {
       onCalendar = "*:0/30";
@@ -8,7 +13,6 @@
         snapshot_dir = "btrbk_snapshots";
         volume."/mnt/raw-nixos" = {
           subvolume = {
-             "/etc/nixos" = {};
              "@home" = {};
           };
           target = "/mnt/raw-backup/backup";
