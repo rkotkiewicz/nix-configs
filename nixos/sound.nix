dfd82b75cda794fb9b5c0c5aefa8c8a462b36799
rkotkiewicz <you@example.com>
2023-03-22 20:06:57 +0100

    switch channels in pipewire

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -8,5 +8,18 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = false;
+    media-session = {
+      enable = true;
+      config.alsa-monitor.rules = [{
+        # Matches all cards
+        matches = [ { "node.name" = "~alsa_output.*"; } ];
+        actions = {
+          "update-props" = {
+            "audio.position" = "FR,FL";
+          };
+        };
+      }];
+    };
   };
 }
