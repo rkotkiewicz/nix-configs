4e17b8fdbe107a96a33e83d7d3cfae2107fe36cd
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2023-06-30 18:34:25 +0200

    change displayManager to gdm

--- nixos/display-server.nix
+++ nixos/display-server.nix
@@ -1,17 +1,18 @@
 { inputs, lib, config, pkgs, ... }: {
 
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.autoNumlock = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+#  services.xserver.displayManager.sddm.autoNumlock = true;
   services.xserver.desktopManager.plasma5.enable = true;
   services.xserver.displayManager.defaultSession = "plasmawayland";
   services.xserver.layout = "pl";
 
   services.xserver.videoDrivers = lib.mkDefault [ "amdgpu" ];
-  services.xserver.displayManager.setupCommands = ''
-    CENTER='DP-0'
-    RIGHT='HDMI-0'
-    ${pkgs.xorg.xrandr}/bin/xrandr --output $CENTER --primary --output $RIGHT --rotate right --right-of $CENTER
-  '';
+#  services.xserver.displayManager.setupCommands = ''
+#    CENTER='DP-0'
+#    RIGHT='HDMI-0'
+#    ${pkgs.xorg.xrandr}/bin/xrandr --output $CENTER --primary --output $RIGHT --rotate right --right-of $CENTER
+#  '';
 
 }
