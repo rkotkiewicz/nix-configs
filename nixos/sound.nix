873d4c5a8d4b0ec2c475eaf496c4c204a9641864
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2023-09-21 21:16:04 +0200

    next try to fix noise from speakers

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -8,5 +8,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
   };
+
+  environment.etc."wireplumber/main.lua.d/90-suspend-timeout.lua" = {
+    text = ''
+      session.suspend-timeout-seconds = 0
+    '';
+  };
+
 }
