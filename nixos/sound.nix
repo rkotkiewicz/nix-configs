f613c6865c43352b8c19a9f22a7980aba70a8d4f
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2023-09-27 21:04:27 +0200

    fix sound

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -13,7 +13,17 @@
 
   environment.etc."wireplumber/main.lua.d/90-suspend-timeout.lua" = {
     text = ''
-      session.suspend-timeout-seconds = 0
+      rule = {
+        matches = {
+          {
+            { "node.name", "matches", "alsa_output.*" },
+          },
+        },
+        apply_properties = {
+          ["session.suspend-timeout-seconds"] = 0,
+        },
+      }
+      table.insert(alsa_monitor.rules, rule)
     '';
   };
 
