be1edb024448fea6e9ce63d70546ba1f3323f5fa
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2024-01-03 22:33:42 +0100

    make filesystem mounts dependent on root

--- nixos/file-systems.nix
+++ nixos/file-systems.nix
@@ -4,9 +4,19 @@
     "/".options = [ "compress=zstd" "discard=async" ];
 #    "/mnt/raw-nixos".options = [ "compress=zstd" "discard=async" ];
 #    "/mnt/raw-backup".options = [ "compress=zstd" "discard=async" ];
-    "/home".options = [ "compress=zstd" "discard=async" ];
+    "/home" = {
+        options = [ "compress=zstd" "discard=async" ];
+        dependsOn = [ "/" ];
+    };
 #    "/home/radek/Games".options = [ "compress=zstd" "discard=async" ];
-    "/nix".options = [ "compress=zstd" "noatime" "discard=async" ];
+    "/nix" = {
+        options = [ "compress=zstd" "discard=async" ];
+        dependsOn = [ "/" ];
+    };
+    "/mnt/magazyn" = {
+        options = [ "compress=zstd" "discard=async" ];
+        dependsOn = [ "/" ];
+    };
 #    "/swap".options = [ "noatime" ];
   };
 #  swapDevices = [ { device = "/swap/swapfile"; } ];
