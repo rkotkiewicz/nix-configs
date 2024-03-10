96e991049ed68b91f781ca96b6a42b775fe23e0e
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2024-03-10 21:41:45 +0100

    update wireplumber config

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -8,24 +8,26 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/main.lua.d/90-suspend-timeout.lua" ''
+          rule = {
+             matches = {
+             {
+                { "node.name", "matches", "alsa_output.*" },
+              },
+            },
+            apply_properties = {
+              ["session.suspend-timeout-seconds"] = 0,
+            },
+          }
+          table.insert(alsa_monitor.rules, rule)
+         '')
+      ];
+    };
     jack.enable = true;
   };
-
-  environment.etc."wireplumber/main.lua.d/90-suspend-timeout.lua" = {
-    text = ''
-      rule = {
-        matches = {
-          {
-            { "node.name", "matches", "alsa_output.*" },
-          },
-        },
-        apply_properties = {
-          ["session.suspend-timeout-seconds"] = 0,
-        },
-      }
-      table.insert(alsa_monitor.rules, rule)
-    '';
-  };
-
 }
+
+
