c1f7a27b64cf6028853edbc545e0fb90a04c36a8
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2025-06-10 13:14:23 +0200

    fix locale

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -4,6 +4,10 @@
   i18n = {
     defaultLocale = "pl_PL.UTF-8";
     extraLocales = [ "pl_PL.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+    extraLocaleSettings = {
+      LANG = "pl_PL.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+    };
   };
 
 }
