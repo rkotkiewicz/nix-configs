860a92a21f424ce411b4f6cc788e7f28cefb1c09
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2025-06-19 17:10:13 +0200

    fix locale, next try

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -3,9 +3,8 @@
   time.timeZone = "Europe/Warsaw";
   i18n = {
     defaultLocale = "pl_PL.UTF-8";
-    extraLocales = [ "pl_PL.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+    extraLocales = [ "en_US.UTF-8/UTF-8" ];
     extraLocaleSettings = {
-      LANG = "pl_PL.UTF-8";
       LC_MESSAGES = "en_US.UTF-8";
     };
   };
