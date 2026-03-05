b3494a8a3a8d7bb643b1209bec6b68c7a6e013f1
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-03-05 10:35:56 +0100

    add otbr

--- hosts/malina/otbr.nix
+++ hosts/malina/otbr.nix
@@ -11,17 +11,11 @@
 
       environment = {
         OT_RCP_DEVICE = "spinel+hdlc+uart:///dev/ttyACM1?uart-baudrate=460800";
-        OT_INFRA_IF = "eth0";
-        OT_THREAD_IF = "wpan0";
-        OT_LOG_LEVEL = "7";
-
-        OTBR_WEB_PORT = "8081";
       };
 
       extraOptions = [
         "--network=host"
         "--cap-add=NET_ADMIN"
-        "--privileged"
         "--device=/dev/ttyACM1"
         "--device=/dev/net/tun"
         "--volume=/var/lib/otbr:/data"
@@ -29,34 +23,11 @@
     };
   };
 
-#  boot.kernel.sysctl = {
-#    "net.ipv6.conf.all.forwarding" = 1;
-#    "net.ipv6.conf.default.forwarding" = 1;
-#  };
 
-  services.avahi = {
-    enable = true;
-    ipv4 = true;
-    ipv6 = true;
-    nssmdns4 = true;
-    nssmdns6 = true;
-    openFirewall = true;
-    publish = {
-      enable = true;
-      userServices = true;
-    };
-  };
+  services.avahi.enable = true;
 
   networking.enableIPv6 = true;
-  boot.kernel.sysctl = {
-    "net.ipv6.conf.all.forwarding" = 1;
-  };
 
-  networking.firewall.enable = false;
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = 1;
 
-#  services.otbr = {
-#    enable = true;
-#    interface = "wpan0"; # The interface for your Thread radio (e.g., USB stick)
-#    radioUrl = "spinel+uart:////dev/serial/by-id/usb-SMLIGHT_SMLIGHT_SLZB-MR5U_SLZB-MR5U234054-if01";
-#  };
 }
