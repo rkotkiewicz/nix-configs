779ce0265082bea789881e6d794aab204b033a6e
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-05-02 20:41:29 +0200

    update firefox config

--- hosts/pc/home-manager/firefox.nix
+++ hosts/pc/home-manager/firefox.nix
@@ -1,7 +1,8 @@
 { config, pkgs, theme, ... }:
 
 {
-    programs.firefox = {
-        enable = true;
-    };
+  programs.firefox = {
+    enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
+  };
 }
