3a62b5b3badcffa56d935d370da7b139d3f6ab0b
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-07-08 15:05:07 +0200

    fix firefox performance

--- hosts/pc/home-manager/firefox.nix
+++ hosts/pc/home-manager/firefox.nix
@@ -4,5 +4,10 @@
   programs.firefox = {
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
+    profiles.default.settings = {
+      "dom.ipc.processCount" = 64;
+      "dom.ipc.processCount.webIsolated" = 32;
+      "browser.tabs.unloadOnLowMemory" = true;
+    };
   };
 }
