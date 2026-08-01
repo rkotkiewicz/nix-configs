92cb7f32598bcea2404d396a545eaefc6dfd3c9e
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-08-01 12:33:22 +0200

    make firefox config more declarative

--- hosts/pc/home-manager/firefox.nix
+++ hosts/pc/home-manager/firefox.nix
@@ -4,10 +4,19 @@
   programs.firefox = {
     enable = true;
     configPath = "${config.xdg.configHome}/mozilla/firefox";
-    profiles.default.settings = {
-      "dom.ipc.processCount" = 64;
-      "dom.ipc.processCount.webIsolated" = 32;
-      "browser.tabs.unloadOnLowMemory" = true;
+    profiles.default = {
+      settings = {
+        "dom.ipc.processCount" = 64;
+        "dom.ipc.processCount.webIsolated" = 32;
+        "browser.tabs.unloadOnLowMemory" = true;
+
+        "signon.rememberSignons" = false;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
+        "widget.use-xdg-desktop-portal.mime-handler" = 1;
+      };
     };
   };
 }
