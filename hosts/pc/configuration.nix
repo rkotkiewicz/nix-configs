329585f5e71877f056035fc6264027edf18c5693
Radosaw Kotkiewicz <radoslaw.kotkiewicz@gmail.com>
2026-09-06 10:22:19 +0200

    fix settings

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -134,7 +134,7 @@
 
     settings = {
       # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
+      experimental-features = ["nix-command" "flakes"];
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
     };
